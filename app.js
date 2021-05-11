4514e17c200dc76f88402611e136d2d984b5ce4a
Kevin Ang <cobrahips@gmail.com>
2021-05-11 12:35:34 -0400

    JS added

---
+++ app.js
@@ -0,0 +1,28 @@
+// Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+
+// Event Handlers
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value !='')
+        AudioDestinationNode(input.value);
+    input.value = '';
+}
+
+// Helpers
+function addTodo(todo) {
+    let ul = document.querySelector('ul')
+    let li = document.createElement('li')
+
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <button name='deleteButton"><i class="fas fa-trash"></i></button>    
+    
+    `;
+
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+
+}
