c80c2be0bebd26f9dcb55cc0595c55beb00b6ec6
Kevin Ang <cobrahips@gmail.com>
2021-05-11 12:54:45 -0400

    fixed typos in JS. In CSS added todo item flex grow and typo fix

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ function handleSubmitForm(e) {
     e.preventDefault();
     let input = document.querySelector('input');
     if (input.value !='')
-        AudioDestinationNode(input.value);
+        addTodo(input.value);
     input.value = '';
 }
 
 // Helpers
 function addTodo(todo) {
-    let ul = document.querySelector('ul')
-    let li = document.createElement('li')
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
 
     li.innerHTML = `
         <span class="todo-item">${todo}</span>
         <button name="checkButton"><i class="fas fa-check-square"></i></button>
-        <button name='deleteButton"><i class="fas fa-trash"></i></button>    
+        <button name="deleteButton"><i class="fas fa-trash"></i></button>    
     
     `;
 
